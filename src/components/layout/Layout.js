aa62a96581934a45311f08d8c366a348993d5b8a
abdelammeer <abdelhamid.ameur@hotmail.de>
2023-09-08 16:52:42 +0200

    t

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -56,7 +56,7 @@ const Layout = ({ children }) => {
       <div>{children}</div>
 
       <div className="footer">
-      <p>
+      {/* <p>
          The Mediterranean Sea as a border, as a place, as a space - for wind, waves and ground, for journeys, for escape, for sea dwellers, cargo ships and narratives, for longing, for memories, for stagnation, for movement and currents.
          
          in this seminar we will take time to reflect, to remember, to read and write, draw and film, experiment on analogue and digital moving image — on a personal level then abstract and connect to global issues, deal with the possibilities to pinpoint a topic for individual or a collective work as well as a presentation method through projections on different materials with the aim visualize in time and space.
@@ -68,7 +68,7 @@ const Layout = ({ children }) => {
          "So what do we mean when we say “water”? Ocean? The rain falling from the sky? The mineral water in the glass? Or ourselves, because our bodies are mostly made of water? Water is omnipresent and the origin of all life. But when we say «water», the water always connects us with something else. We mean reflections on the water or what we suspect beneath its surface. We mean travel memories, the habitat of plants and animals and the weather. We mean environmental disasters, water shortages and the drinking water trade. And we mean global economy, human rights and their violations in the Mediterranean. The water leads us to complex topics that affect culture, nature, politics, economy, body and mind." — Roni Horn
 
          “I see everything,” she says, as if it was a curse. Brilliant sunshine, clear blue skies. The sea is calm, framed by a piece of railing. Buzzing voices. A peaceful moment if it weren’t for the fact that the sea is standing upright, vertical, like a waterfall. A rush of images, twirling, upside down, jolting. People in the boat, in the water, screams, life jackets, emergency whistles. Fluorescent orange, geometrical shapes cast by the sun. There’s no horizon any more, no sky, no up or down, only deepness and nothing to hold on to. Even time’s flow comes to a halt, contracting into the brutal present. She is filming and speaking. To him, to herself, to us, perhaps. Floating legs in sweat pants, jeans, thronged together. A blouse with butterflies, it looks like their wings are flapping in the water. The snake-like belt of a coat, a crumpled-up plastic cup, a pack of cigarettes. Fuck you all! She speaks, she rages, and she films to beat being tired, being cold, the fact that help isn’t coming. To beat dying, just for something to remain. — Amel Alzakout
-      </p>
+      </p> */}
         <div className="contact">
           <div className="title"><h1>Logo</h1></div>
           <div className="adress"><h2>Strohberg 20, 70180 Stuttgart</h2></div>
