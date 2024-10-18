489b842dbcd5a9aa593f8dfcd0f5e98155aae549
abdelammeer <abdelhamid.ameur@hotmail.de>
2024-10-18 23:18:03 +0200

    c

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -141,6 +141,9 @@ export const pageQuery = graphql`
                   gatsbyImageData
                 }
               }
+              videoFile {
+                publicURL
+              }
             }
             title_section {
               title
